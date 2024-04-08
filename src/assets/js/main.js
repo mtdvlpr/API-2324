c9b2021dfe498d97ac7cbbcd6f298add700789e4
Manoah Tervoort <149895ja@gmail.com>
2024-04-08 15:15:06 +0200

    feat: index page

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,4 +1,22 @@
 import 'vite/modulepreload-polyfill'
 import './../css/main.scss'
 
-console.log('hello world')
+console.log('JS init')
+
+const shareButtons = document.querySelectorAll('[data-share]')
+shareButtons.forEach((btn) => {
+  btn.addEventListener('click', async (e) => {
+    try {
+      const movie = JSON.parse(e.target.getAttribute('data-share'))
+      console.log('data', movie)
+      await navigator.share({
+        title: movie.title,
+        text: 'Check out this awesome movie!',
+        url: `${window.location.host}/movie/${movie.id}`,
+      })
+      console.log('Data was shared successfully', movie)
+    } catch (e) {
+      console.error(e)
+    }
+  })
+})
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
+import 'dotenv/config'
 import { App } from '@tinyhttp/app'
 import { logger } from '@tinyhttp/logger'
 import sirv from 'sirv'
+import fetch from 'node-fetch'
 
 import { getAbsolutePath, isProduction } from './utils/general'
 import { renderTemplate } from './utils/renderer'
@@ -12,8 +14,33 @@ if (isProduction) {
   app.use('/assets', sirv(getAbsolutePath('dist', 'assets')))
 }
 
-app.get('/', (_, res) => {
-  return renderTemplate(res, 'index', { name: 'Liquid' })
+app.get('/', async (_, res) => {
+  let movies = []
+  try {
+    const response = await fetch(
+      `https://api.themoviedb.org/3/movie/popular?language=en-US&api_key=${process.env.MOVIEDB_TOKEN}`
+    )
+    const result = (await response.json()) as any
+    movies = result.results
+  } catch (e) {
+    console.error(e)
+  }
+  return renderTemplate(res, 'index', { movies })
+})
+
+app.get('/movie/:id/', async (req, res) => {
+  const movieID = req.params.id
+  let movie = {}
+  try {
+    const response = await fetch(
+      `https://api.themoviedb.org/3/movie/${movieID}?api_key=${process.env.MOVIEDB_TOKEN}`
+    )
+    const result = (await response.json()) as any
+    movie = result
+  } catch (e) {
+    console.error(e)
+  }
+  return renderTemplate(res, 'detail', { title: 'Movie', movie })
 })
 
 app.listen(3000, () => console.log('Listening on http://localhost:3000'))
