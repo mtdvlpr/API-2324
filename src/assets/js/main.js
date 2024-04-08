7f699e69bc84e2ea979f63f987fc9be9db0a9992
Manoah Tervoort <149895ja@gmail.com>
2024-04-08 16:43:39 +0200

    feat: share api

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -5,18 +5,20 @@ console.log('JS init')
 
 const shareButtons = document.querySelectorAll('[data-share]')
 shareButtons.forEach((btn) => {
-  btn.addEventListener('click', async (e) => {
-    try {
-      const movie = JSON.parse(e.target.getAttribute('data-share'))
-      console.log('data', movie)
-      await navigator.share({
-        title: movie.title,
-        text: 'Check out this awesome movie!',
-        url: `${window.location.host}/movie/${movie.id}`,
-      })
-      console.log('Data was shared successfully', movie)
-    } catch (e) {
-      console.error(e)
-    }
-  })
+  if (window.navigator?.share) {
+    btn.addEventListener('click', async (e) => {
+      try {
+        const movie = JSON.parse(e.target.getAttribute('data-share'))
+        await navigator.share({
+          title: movie.title,
+          text: 'Check out this awesome movie!',
+          url: `${window.location.origin}/movie/${movie.id}`,
+        })
+      } catch (e) {
+        console.error(e)
+      }
+    })
+  } else {
+    btn.parentElement.removeChild(btn)
+  }
 })
